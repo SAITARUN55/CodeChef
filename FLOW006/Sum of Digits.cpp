4e2447b5d99f9e91cee6156b829c14c862821ea3
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-04-26 22:13:59 +0530

    Create Sum of Digits.cpp

---
+++ FLOW006/Sum of Digits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int m,n,sum=0;
+	    cin>>n;
+	    while(n!=0){
+	        m=n%10;
+	        sum=sum+m;
+	        n=n/10;
+	    }
+	    cout<<sum<<"\n";
+	}
+	return 0;
+}
