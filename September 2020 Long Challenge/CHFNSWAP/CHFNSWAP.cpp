7a82f6e2cddd13eef87324fdc5bd343b1a293aba
Sai Tarun Kaniganti <saitarunkaniganti@gmail.com>
2020-09-17 12:29:57 +0530

    Create CHFNSWAP.cpp

---
+++ September 2020 Long Challenge/CHFNSWAP/CHFNSWAP.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+ll pos(ll sm)
+{
+    long double a=sqrt(1ul+4*(sm));
+    a=a-1.0;
+    a=a/2.0;
+    ll fs=a;
+    return fs;
+}
+int main() {
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+	    ll n;
+	    cin>>n;
+	    if(n==0 || n==1 || n==2)
+	    {
+	        cout<<0<<endl;
+	        continue;
+	    }
+	    ll t1=(n*(n+1))/2;
+	    if(t1%2!=0)
+	    {
+	        cout<<0<<endl;
+	        continue;
+	    }
+	    ll indx=pos(t1);
+	    ll ss=indx*(indx+1)/2;
+	    if(t1/2==ss)
+	    {
+	        ll ans=(indx*(indx-1ul))/2l+((n-indx)*(n-indx-1ul))/2l+(n-indx);
+	        cout<<ans<<endl;
+	    }
+	    else
+	    cout<<n-indx<<endl;
+	}
+	return 0;
+}
