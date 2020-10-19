581ad1a96e9410e878f39ae33862a922cd482b04
Sai Tarun Kaniganti <saitarunkaniganti@gmail.com>
2020-10-19 21:40:13 +0530

    Create CHEFEZQ.cpp

---
+++ October 2020 Long Challenge/CHEFEZQ/CHEFEZQ.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#define ll long long int
+
+int main() {
+	ll t;
+	cin>>t;
+	while(t--){
+	    ll n,k,t=0;
+	    cin>>n>>k;
+	    ll arr[n];
+	    for(int i=0;i<n;i++)
+	        cin>>arr[i];
+	    ll i;
+	    for(i=0;i<n;i++){
+	        if(arr[i]>=k){
+	            t++;
+	            arr[i+1]+=(arr[i]-k);
+	        }
+	        if(arr[i]<k)
+	            break;
+	    }
+	    if(i==n)
+	        t+=((arr[n-1]-k)/k);
+	        cout<<t+1<<endl;
+	}
+	return 0;
+}
